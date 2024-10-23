94826cf7064bbf5e4ecb602d7b9a645e814176dd
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-10-22 17:03:09 -0700

    recompile

--- ecoscope-workflows-patrols-workflow/ecoscope_workflows_patrols_workflow/dags/run_sequential.py
+++ ecoscope-workflows-patrols-workflow/ecoscope_workflows_patrols_workflow/dags/run_sequential.py
@@ -1,6 +1,6 @@
 # [generated]
 # by = { compiler = "ecoscope-workflows-core", version = "9999" }
-# from-spec-sha256 = "d0d6fbaebf97df068d2a82cc842bda1ceb49040d757cbcf4f8e7ac99b3e74f2f"
+# from-spec-sha256 = "87335a226da9d7e7ce8772f162fe40a8dcdae0dc6ab355c106532d06fc07141f"
 import json
 import os
 
