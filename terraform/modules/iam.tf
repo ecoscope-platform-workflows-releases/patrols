120fbf456249630ab7926a6cd83f0c6e2c6a1e23
andersondario-er <andersona@earthranger.com>
2025-06-19 11:14:08 -0300

    Reusing var

--- terraform/modules/iam.tf
+++ terraform/modules/iam.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "default" {
-  account_id = startswith(var.env, "dev-preview")? "wf-patrols-${var.env}" : "workflow-patrols-${var.env}"
+  account_id = startswith(var.env, "dev-preview")? "wf-${var.application_short_name}-${var.env}" : "${var.application_name}-${var.env}"
   project    = var.project_id
 }
 
