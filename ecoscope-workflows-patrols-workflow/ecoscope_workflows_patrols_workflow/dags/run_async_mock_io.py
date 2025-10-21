adfb78e58c084c8f3f9f5b99a920123e71f09e1f
ecoscope-elebot <189163401+ecoscope-elebot@users.noreply.github.com>
2025-10-21 20:16:29 +0000

    auto: recompile ecoscope-workflows-patrols-workflow

--- ecoscope-workflows-patrols-workflow/ecoscope_workflows_patrols_workflow/dags/run_async_mock_io.py
+++ ecoscope-workflows-patrols-workflow/ecoscope_workflows_patrols_workflow/dags/run_async_mock_io.py
@@ -903,6 +903,7 @@ def main(params: Params):
                 "north_arrow_style": {"placement": "top-left"},
                 "legend_style": {
                     "title": DependsOn("set_patrol_traj_color_column"),
+                    "format_title": True,
                     "placement": "bottom-right",
                 },
                 "static": False,
@@ -1728,7 +1729,11 @@ def main(params: Params):
             partial={
                 "tile_layers": DependsOn("base_map_defs"),
                 "north_arrow_style": {"placement": "top-left"},
-                "legend_style": {"title": "Time Spent", "placement": "bottom-right"},
+                "legend_style": {
+                    "title": "Time Spent",
+                    "format_title": False,
+                    "placement": "bottom-right",
+                },
                 "static": False,
                 "title": None,
                 "max_zoom": 20,
